942c277231f05ac5527019f346351ba43b2ff74e
rosem <rosem@0_o.centre.edu>
2024-10-15 18:55:31 -0400

    done

--- src/main/java/PaintBrush.java
+++ src/main/java/PaintBrush.java
@@ -27,7 +27,7 @@ set the "paint" for the paintbrush
 */	
 	public void setPaint(Paint paint)
 	{
-   
+		this.paint = paint; 
    
 	}
 
@@ -37,7 +37,7 @@ set the "paint" for the paintbrush
 */
 	public Paint getPaint()
 	{
-		return Gold;
+		return this.paint;
 	}
 	
    
@@ -46,7 +46,7 @@ set the "paint" for the paintbrush
    */
 	public void setBrighter()
 	{		
-
+		this.paint = new PaintBrighter(paint);
 	}
 
 
@@ -55,7 +55,7 @@ set the "paint" for the paintbrush
    */
 	public void setDarker()
 	{
-		
+		this.paint = new PaintDarker(paint);
 	}
 
 
@@ -64,6 +64,72 @@ set the "paint" for the paintbrush
    */
 	public void paint(int x, int y, Paint[][] mesh)
 	{
+		if (mode == BrushMode.paintMode)
+		{
+			mesh[x][y] = this.paint;//ask him about this
+		}
+		else if(mode == BrushMode.fillMode)
+		{
+			PaintColor c = (PaintColor) mesh[x][y]; //why is PaintColor like this?
+			paintFill(x,y,mesh,c, this.paint);
+		}
+		else if(mode == BrushMode.pattern1Mode)
+		{
+			Paint pink = new PaintColor(Color.PINK);
+			PaintColor c = (PaintColor) mesh[x][y];
+			paintFill(x,y,mesh,c, pink);
+			c = (PaintColor) mesh[x][y]; //why is PaintColor like this?
+			paintPattern1(x,y,mesh,c);
+		}
+	}
+	public void paintFill(int x, int y, Paint[][] mesh, Paint c, Paint pc)
+	{
+		mesh[x][y] = pc;
+		if (y+1 < 400/8 && c.equals(mesh[x][y+1]))
+		{
+			paintFill(x, y+1, mesh, c, pc);
+		}
+		if (y-1 >= 0 && c.equals(mesh[x][y-1]))
+		{
+			paintFill(x, y-1, mesh, c, pc);
+		}
+		if (x-1 >= 0 && c.equals(mesh[x-1][y]))
+		{
+			paintFill(x-1, y, mesh, c, pc);
+		}
+		if (x+1 < 400/8 && c.equals(mesh[x+1][y]))
+		{
+			paintFill(x+1, y, mesh, c, pc);
+		}
+	}
+	public void paintPattern1(int x, int y, Paint[][] mesh, Paint c)
+	{
+		if (x%2 == 0)
+		{
+			PaintColor p = new PaintColor(Color.YELLOW);
+			mesh[x][y] = p;
+		}
+		else 
+		{
+			PaintColor p = new PaintColor(Color.WHITE);
+			mesh[x][y] = p;
+		}
+		if (y+1 < 400/8 && c.equals(mesh[x][y+1]))
+		{
+			paintPattern1(x, y+1, mesh, c);
+		}
+		if (y-1 >= 0 && c.equals(mesh[x][y-1]))
+		{
+			paintPattern1(x, y-1, mesh, c);
+		}
+		if (x-1 >= 0 && c.equals(mesh[x-1][y]))
+		{
+			paintPattern1(x-1, y, mesh, c);
+		}
+		if (x+1 < 400/8 && c.equals(mesh[x+1][y]))
+		{
+			paintPattern1(x+1, y, mesh, c);
+		}
 		
 	}
 
